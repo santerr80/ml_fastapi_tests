c6089f2e170d6456bccdd47ab88de2a39fdae764
Александр Горбунов <santerr80@mail.ru>
2024-04-14 19:06:50 +0700

    Update main.py
    Добавил загрузку сервера Uvicorn
    Импортировал необходимые библиотеки

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 from fastapi import FastAPI
 from transformers import pipeline
 from pydantic import BaseModel
+import uvicorn
+import asyncio
 
 
 class Item(BaseModel):
     text: str
 
 
-
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
 
-
 @app.get("/")
-def root():
+async def root():
     return {"message": "Hello World"}
 
 
 @app.post("/predict/")
-def predict(item: Item):
+async def predict(item: Item):
     return classifier(item.text)[0]
+
+
+# Запуск сервера Uvicorn
+async def main():
+    config = uvicorn.Config("main:app", port=8000, log_level="info")
+    server = uvicorn.Server(config)
+    await server.serve()
+
+if __name__ == "__main__":
+    asyncio.run(main())
